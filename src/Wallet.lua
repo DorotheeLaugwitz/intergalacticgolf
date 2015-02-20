e995a60b02ce675ced5e9b0a327d1cb51009d107
Dorothee Laugwitz <dorothee.laugwitz@gmail.com>
2015-02-20 13:16:33 +0100

    added Wallet

---
+++ src/Wallet.lua
@@ -0,0 +1,26 @@
+require ("lib.lclass")
+
+class "Wallet"
+
+function Wallet:Wallet()
+  self.balance = 250
+end
+
+function Wallet:balance ()
+  return math.floor(self.balance)
+end
+
+function Wallet:balanceAsString ()
+  return tostring(math.floor(self.balance))
+end
+
+function Wallet:add (number)
+  self.balance = self.balance + number
+  return self.balance
+end
+
+function Wallet:withdraw (number)
+  self.balance = self.balance - number
+  return self.balance
+end
+
